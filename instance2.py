bd9c5b6e3986c148b36a067a58920e1dec1756c5
Norman <thenormanmu@gmail.com>
2016-04-23 20:40:45 -0700

    instance 2 done

---
+++ instance2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random, math
+
+f = open('instance2.in', 'w')
+
+f.write('498\n')
+s = ''
+for i in range(498):
+    if random.random() > 0.5:
+        s += '%d ' % i
+
+s = s[:-1] + '\n'
+
+f.write(s)
+
+graph = np.zeros((500, 500))
+
+for i in range(0, 498, 6):
+    for j in range(i, i+5):
+        for k in range(j+1, i+6):
+            if random.random() <= 0.85:
+                graph[j][k] = 1
+            if random.random() <= 0.85:
+                graph[k][j] = 1
+        graph[j][j+1] = 1
+    graph[i][i+6] = 1
+
+for i in range(500):
+    s = ''
+    for j in range(500):
+        if graph[i][j]:
+            s += '1 '
+        else:
+            s += '0 '
+    s = s[:-1] + '\n'
+    f.write(s)
+
+f.close()
