1f765719e0dc4e29c6d77c7fcec3ae8df75d55b8
Norman <thenormanmu@gmail.com>
2016-04-27 22:23:17 -0700

    cleaned up code, moved everything to solver.py

--- solver/solver.py
+++ solver/solver.py
@@ -1,70 +1,200 @@
+import sys
+import collections
+import random
+import Queue
+import operator
+import scc
 
-import sys, collections, itertools
+CYCLES_PER_NODE = 10
+DP_MAX_SIZE = 20
+SKIPPED = (12, 15, 50, 102, 128, 154, 219, 238, 258, 352, 369, 409, 418, 429, 465)
+SEARCH_ITERATIONS = 10
 
-
-def main(argv):
-    if len(argv) != 2:
-        print "Please specify input filename and number of iterations"
-        return
-    else:
-        graph, num_nodes = parse(argv[0])
-        return solve(graph, num_nodes, argv[1])
-
-def parse(filename):
+#creating graph
+def read_graph(filename):
     f = open("instances/" + filename, 'r')
-    
     #create graph
     graph = []
     num_nodes = int(f.readline())
-    children = f.readline().split()
-<<<<<<< 0471dace119a78efa4097152a01add0deb56ac56
+    children = set(f.readline().split())
+    children = map(int, children)
     for _ in range(num_nodes):
-        line = f.readline().split()
-        tmp = set()
+        s = f.readline().split()
+        tmp = []
         for i in range(num_nodes):
-            if line[i]:
-                tmp.add(i)
+            if s[i] == '1':
+                tmp += [i]
         graph.append(tmp)
-=======
-    for i in range(num_nodes):
-        f.readline().split())
->>>>>>> Added random solution generator file(incomplete)
+    return graph, num_nodes, children
 
-    return graph, num_nodes
+def main(argv):
+    if len(argv) != 1:
+        print("Missing argument")
+    else:
+        if argv[0] == "all":
+            for instance in range(1, 493):
+                if instance not in SKIPPED:
+                    solve(instance)
+        else:
+            solve(argv[0])
 
-def solve(graph, num_nodes, iterations):
-    #find <5-cycles
-    cycles = {}
-    for i in range(5, 6):
-        #l should be list of i-cycles
-        #[(1, 2, 3), ...]
-        l = []
-        for j in range(num_nodes):
-            #we always iterate through nodes in numerical order
-            #when calling cycle() on node i, we only consider cycles using nodes >i
-            l.extend(cycle(j, i, graph, num_nodes))
+def solve(instance):
+    graph, num_nodes, children = read_graph("%d.in" % instance)
+    nodes_left = [i for i in range(num_nodes)]
+    components = scc.scc(graph)
+    solution = []
+    for c in components:
+        if not acyclic(c):
+            if len(c) > DP_MAX_SIZE:
+                best_solution = None
+                best_penalty = float("inf")
+                for _ in range(SEARCH_ITERATIONS):
+                    solution, leftovers = random_solution(graph, component, children)
+                    local_best = local_search(graph, c, children, solution, leftovers)
+                    local_penalty = penalty(graph, local_best, children)
+                    if local_penalty < best_penalty:
+                        best_solution = local_best
+                        best_penalty = local_penalty
+                solution.extend(best_solution)
+            else:
+                solution.extend(dynamic_programming())
 
-        cycles[i] = l
+    print("Penalty for instance %d: %d" % (instance, penalty(graph, solution, children)))
+
+def acyclic(graph, component):
+    S = collections.deque()
+    S.append(next(iter(s)))
+    visited = set()
+    while S:
+        current = S.pop()
+        if current in visited:
+            return False
+        else:
+            visited.add(current)
+            for neighbor in graph[current]:
+                if neighbor in component:
+                    S.append(neighbor)
+    return True
+
+def dynamic_programming():
+    return None
+
+def local_search(graph, component, children, solution, leftovers):
+    current_penalty = penalty(graph, current, children)
     
-    #returns cycles for now for testing
+    while True:
+        best_neighbor = None
+        best_penalty = float("inf")
+        for cycle in current:
+            nodes_left = cycle | leftovers
+            neighbor = dynamic_programming()
+            if not best_neighbor or penalty(graph, neighbor, children) < best_penalty:
+                best_neighbor = neighbor
+                best_penalty = penalty(graph, neighbor, children)
+
+        if best_penalty >= current_penalty:
+            break
+        else:
+            current = best_neighbor
+            current_penalty = best_penalty
+
+    return current
+
+def random_solution(graph, nodes_left, children):
+    #for now nodeIterationOrder is completely random
+    order = iteration_order(graph)
+    for node in order:
+          if node in nodes_left:
+              #finding cycles starting at that node
+            cycles = find_cycles_dfs(graph, node, nodes_left)
+               #choosing a completely random cycle
+            cycle = choose_cycle(cycles, children)
+            #once we've incorporated a cycle into the solution, we remove the nodes from consideration
+            for nd in cycle:
+                nodes_left.remove(nd)
+    return penalty(graph, nodes_left, children)
+    #return cycles-used, leftovers
+
+#todo: fix penalty
+def penalty(graph, solution, children):
+    penalty = 0
+    for cycle in solution:
+        for node in cycle:
+            if node in children:
+                penalty += 2
+            else:
+                penalty +=1
+    return penalty
+
+def iteration_order(graph):
+    num_nodes = len(graph)
+    nodes = [i for i in range(0,num_nodes)]
+    weightVector = assign_weights(graph, nodes)
+    sortedWeightVector = sorted(weightVector.items(), key=operator.itemgetter(1))
+    nodeIterationOrder = []
+    #print(sortedWeightVector)
+    for tup in sortedWeightVector:
+        nodeIterationOrder += [tup[0]]
+    return nodeIterationOrder
+
+
+def assign_weights(graph, nodes):
+    weightVector = {}
+    for node in nodes:
+        weightVector[node] = 0
+        for n in graph[node]:
+            weightVector[node] += 1
+    return weightVector
+
+def find_cycles_dfs(graph, node, nodes_left):
+    cycles = []
+    num_cycles = 0
+    S = collections.deque()
+    S.append([node])
+    while S and num_cycles < CYCLES_PER_NODE:
+        current = S.pop()
+        tail = current[-1]
+        if node in graph[tail]:
+            cycles.append(current)
+            num_cycles += 1
+        if len(current) <= 4:
+            for neighbor in graph[tail]:
+                if neighbor not in current and neighbor in nodes_left and neighbor != node:
+                    S.append(current + [neighbor])
     return cycles
 
-def cycle(node, length, graph, num_nodes):
-    retval = []
-    S = collections.deque() 
-    S.append([node])
-    
-    while S:
-        v = S.pop()
-        if len(v) == length:
-            if node in graph[v[-1]]:
-                retval.append(v)
-        else:
-            for adj in graph[v[-1]]:
-                if adj > node:
-                    S.append(v + [adj])
+def bfs(graph, node, nodes_left):
+    #list of cycles for a particular node
+    cycles = []
+    q = Queue.Queue()
+    q.put([node])
+    while q.qsize() > 0:
+        #current list of nodes in a prospective cycle
+        currentThread = q.get()
+        #accessing the tail
+        tail = currentThread[-1]
+        #checking if the tail has a backedge
+        if node in graph[tail]:
+            cycles.append(currentThread)
+        if len(currentThread) <= 4:
+            for neighbor in graph[tail]:
+                if neighbor not in currentThread and neighbor in nodes_left:
+                    q.put(currentThread + [neighbor])
+    return cycles
 
-    return retval
+def choose_cycle(cycles, children):
+    cycle = []
+    if len(cycles) != 0:
+        cycle = random.choice(cycles)
+    return cycle
+
+def sample(dictionary, range):
+    r = random.random(0, range)
+    s = 0
+    for key in dictionary.keySet():
+        s += dictionary.get(k)
+        if r <= s:
+            return dictionary.get(k)
 
 if __name__ == "__main__":
-    main(sys.argv[1:]) 
+    main(sys.argv[1:])
