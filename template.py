35cc5006994ceee71901ae921fa7b20638823ec0
Ravneet Singh Madaan <ravneet.sm@berkeley.edu>
2016-04-27 20:24:05 -0700

    Fixed DFS, BFS, commented out pseudocode and fixed miscellaneous errors

---
+++ template.py
@@ -0,0 +1,21 @@
+def constructGraph(.....):
+
+
+
+def generateRandomSolution(....):
+
+
+def detectCycles(....):
+
+def localSearch(....):
+
+
+def main(....)
+	g = constructGraph("")
+	cycles = detectCycles()
+	s = generateRandomSolution()
+	return localSearch(s)
+
+
+
+main()
